dbc07e5ed696bed0ab44acb78e90918c8b98b4c5
MBaranErcan <mustafabaranercan23@gmail.com>
2024-02-07 22:30:03 +0300

    Added EBO and wireframe(line)/fill mode

--- ReGL/Main.cpp
+++ ReGL/Main.cpp
@@ -115,28 +115,34 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 
-	// -------------------TRIANGLE-------------------
-	// Triangle vertices
+	// -------------------RECTANGLE-------------------
+	// vertices
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f, // Top left
 		0.5f, 0.5f, 0.0f, // Top right
 		0.5f, -0.5f, 0.0f, // Bottom right
+		-0.5f, 0.5f, 0.0f // Bottom left
 	};
+	unsigned int indices[] = {	// Instead of drawing the rectangle with 6 vertices, we can draw it with 2 triangles using 4 vertices.
+			0, 1, 2, // First Triangle
+			0, 1, 3 // Second Triangle
+		};
 
 
-
-	// -------------------VBO & VAO-------------------	
+	// -------------------VBO & VAO & EBO-------------------	
 	// Vertex Buffer Object (VBO) can store a large number of vertices in the GPU's memory so we can render a large object quickly.
-	unsigned int VBO, VAO;
+	// Vertex Array Object (VAO) can store the configuration of vertex attributes (like pointers to vertex attributes in the VBO) and which VBO to use.
+	// Element Buffer Object (EBO) is a buffer, just like a vertex buffer object, that stores indices that OpenGL uses to decide what vertices to draw.
+	unsigned int VAO, VBO, EBO; // Create a VBO, VAO, and EBO.
+	glGenVertexArrays(1, &VAO); // Generate 1 vertex array object and store the resulting identifier in VAO.
 	glGenBuffers(1, &VBO); // Generate 1 buffer and store the resulting identifier in VBO.
-	glGenVertexArrays(1, &VAO); // Generate 1 buffer and store the resulting identifier in VAO.
+	glGenBuffers(1, &EBO); // Generate 1 buffer and store the resulting identifier in EBO.
 
 	// We want to bind to the VAO first. This is because the subsequent VBO calls we make are then stored inside the VAO.
 	glBindVertexArray(VAO); // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 
 	// Then we bind to the buffer using the GL_ARRAY_BUFFER target. Since we've generated a buffer, we want to bind to it so we can perform operations on it.
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
 	// We pass the data to the buffer using the glBufferData function. The GL_ARRAY_BUFFER target is used to specify the type of buffer we want to copy the data into.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	/*The fourth parameter specifies how we want the graphics card to manage the given data. This can take 3 forms:
@@ -144,6 +150,11 @@ int main() {
 	GL_STATIC_DRAW : the data is set only once and used many times.
 	GL_DYNAMIC_DRAW : the data is changed a lot and used many times.
 	*/
+
+
+	// Bind the EBO and send the data to the buffer.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Bind the EBO.
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // Send the data to the buffer.
 
 
 	// Specify how OpenGL should interpret the vertex data before rendering.
@@ -154,16 +165,21 @@ int main() {
 	// The fourth parameter specifies if we want the data to be normalized. If we're inputting integer data types (unsigned byte, short, etc.), we need to normalize the data to 0 (or -1 for signed data) and 1. We're using GL_FLOAT so we don't want to normalize the data.
 	// The fifth parameter specifies the stride between consecutive vertex attributes. Since the next set of position data is located exactly 3 times the size of a float away, we specify 3 * sizeof(float) as the stride.
 	// The last parameter specifies the offset of where the position data begins in the buffer. Since the position data is at the start of the data array, we specify 0.
-
 	glEnableVertexAttribArray(0); // Enable the vertex attribute at location 0.
+
 
 	// Unbind the VBO and VAO. This is good practice so we don't accidentally modify the VBO and VAO while we're not using them.
 	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the VBO.
 	glBindVertexArray(0); // Unbind the VAO.
 
 
+	// Wireframe & Fill modes
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // uncomment to draw in wireframe mode.
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Draw in fill mode.
 
-	// Render loop
+
+
+	// RENDER LOOP
 	while (!glfwWindowShouldClose(window)) { // Check if the window should close, if not, render the next frame.
 
 		processInput(window); // Check if the user has pressed the escape key, if so, close the window.
@@ -172,10 +188,11 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT); // Clear the screen with the set color.
 	
 
-		// Draw the triangle
+		// Draw the rectangle
 		glUseProgram(shaderProgram); // Use the shader program object.
 		glBindVertexArray(VAO); // Bind the VAO.
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Draw the triangle.
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Draw the rectangle using the VAO and EBO.
+		glBindVertexArray(0); // Unbind the VAO.
 
 
 		glfwPollEvents(); // Check if any events are triggered (like keyboard input or mouse movement events).
@@ -183,15 +200,19 @@ int main() {
 	
 	}
 
+
 	// De-allocate all resources once they've outlived their purpose.
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	// Clean up
 	glfwTerminate(); // Clean up the resources we've allocated.
 	return 0;
+
 }
+
 
 // USER INPUT
 void processInput(GLFWwindow* window) {
